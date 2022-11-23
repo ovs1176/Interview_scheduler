5feb1c6ea1cd3472b9c1f1792d556c8e5e2a9173
ovs1176 <64677914+ovs1176@users.noreply.github.com>
2022-11-23 22:30:29 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+import { Home } from './components/Home';
+import NoteState from './context/notes/NoteState';
+
+function App(){
+  return (
+    <>
+      <NoteState>
+        <Router>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+      </NoteState>
+    </>
+  );
+}
+
+export default App;
